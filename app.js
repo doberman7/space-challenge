74b9a2c709720bd3d0de942595d5fcefc02e0399
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-16 12:55:39 -0600

    sss

--- app.js
+++ app.js
@@ -52,7 +52,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Space Demons 3';
+app.locals.title = 'Space Demonssss 3 ';
 //rutas
 const authRoutes = require('./routes/authRoutes');
 app.use('/', authRoutes);
