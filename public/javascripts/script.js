3c2822dc56a834354a1fe5366f40d8d59d35a95f
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-17 20:12:36 -0600

    comented sttuff ffrom axioos

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -4,15 +4,16 @@ document.addEventListener('DOMContentLoaded', () => {
   const form = document.querySelector('form');
 
   const formEvent = form.addEventListener('submit', async event => {
-    event.preventDefault();
+    // event.preventDefault();
 
     const title = document.querySelector('#createChallenge').value;
     const userId = document.querySelector('#createChallenge').value;
 
-    const todo = {
-      title,
-      userId
-    };
+    // const todo = {
+    //   email,
+    //   time
+    // };
+    // console.log(todo);
 
     const addedTodo = await addTodo(todo);
     addTodosToDOM(addedTodo);
@@ -21,11 +22,11 @@ document.addEventListener('DOMContentLoaded', () => {
   export const addTodo = async todo => {
   // module.exports let addTodo = async todo => {
     try {
-      const res = await axios.post(`/create`, todo);
+      // const res = await axios.post(`/create`, todo);
       // const addedTodo = res.data;
 
       // console.log(`Added a new Todo!`, addedTodo);
-      console.log(`Added a new Todo!`);
+      // console.log(`Added a new Todo!`);
 
     } catch (e) {
       console.error(e);
