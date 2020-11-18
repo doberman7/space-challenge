1faeeccd91cc3630612caa5195a1cd5bfdf8df2b
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-18 14:48:04 -0600

    edited buttons

--- controllers/gameControllers.js
+++ controllers/gameControllers.js
@@ -3,10 +3,6 @@ const Game = require('../models/Game')
 
 exports.ViewCreateGame = (req, res) => res.render('games/game')
 exports.CreateGame = async(req, res) => {
-    let {user} = req.session.passport
-    console.log(user);
-
-    // const { score, time} = req.body
-    // await Game.create({ score, time})
+    
     res.render('games/game')
 }
