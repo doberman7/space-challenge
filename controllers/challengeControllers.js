b27d0605bb0137a9c6a05764e9ebcbef614ae87f
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-18 12:38:48 -0600

    need email

--- controllers/challengeControllers.js
+++ controllers/challengeControllers.js
@@ -1,38 +1,55 @@
 const Challenge = require('../models/Challenge.Model')
 
 
-exports.createChallenge = async(req, res) => {
-    let {user} = req.session.passport
-    let {time,email,score} = req.body
-    if (time != "" && email != "" && user != "" && score != "" ) {
-      console.log("DATA:",time,email,user);
+exports.createChallenge = async (req, res) => {
+  let {
+    user
+  } = req.session.passport
+  let {
+    time,
+    email,
+    score
+  } = req.body
+  //si formularios no vacios
+  if (
+    time != "" &&
+    email != "" &&
+    score != ""
+  ) {
 
-    Challenge.create({
+  if (typeof time == String || typeof score == String) {
+    return res.render('games/game', {
+      infoFlash: "challenge NOT created, use numbers"
+    })
+  }
+
+
+
+    await Challenge.create({
       idChallenger: user,
       userChallenged: email,
       time: time,
       score: score,
       hasBeenBeated: false
     })
-    .then(()=>{
 
-
-      console.log("CREATION SUCCESFULL")
-      //si el challenge puede ser creado
-      return res.render('challenges/challengeList',{infoFlash: "challenge Created"})
-    }).catch(err=>{
-      console.log("DURING CREATION:",err);
+    const challenges = await Challenge.find()
+    //si el challenge puede ser creado
+    return res.render('challenges/challengeList', {
+      infoFlash: "challenge Created",
+      challenges: challenges
     })
+
   } else {
-
-    return res.render('games/game', {infoFlash: "challenge NOT created, missing info"})
-
+    return res.render('games/game', {
+      infoFlash: "challenge NOT created, missing info"
+    })
   }
 
 }
 
 
-exports.readAllChallenges = async(req,res)=>{
+exports.readAllChallenges = async (req, res) => {
   console.log(req.session);
   // Challenge.find({
   //   email
@@ -41,10 +58,10 @@ exports.readAllChallenges = async(req,res)=>{
 }
 
 
-exports.updateChallenge = async(req,res)=>{
+exports.updateChallenge = async (req, res) => {
 
 }
 
-exports.deleteChallenge = async(req,res)=>{
+exports.deleteChallenge = async (req, res) => {
 
 }
