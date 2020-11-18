58e569f101781bac2ff976337fc2aa280d8b6549
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-17 19:34:54 -0600

    flash Challenge created

--- controllers/challengeControllers.js
+++ controllers/challengeControllers.js
@@ -3,9 +3,9 @@ const Challenge = require('../models/Challenge.Model')
 
 exports.createChallenge = async(req, res) => {
     let {user} = req.session.passport
-    console.log(req.session);
+    // console.log(req.session);
 
     // const { score, time} = req.body
     // await Game.create({ score, time})
-    res.render('games/game')
+    res.render('games/game',{infoFlash: "challenge Created"})
 }
