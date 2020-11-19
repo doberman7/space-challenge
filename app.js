20c3e670bc1d76c152ad94d764ac8147f73a748f
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-18 19:07:24 -0600

    ediited title

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 
 // default value for title local
-app.locals.title = 'Doing game';
+app.locals.title = 'Space Challenge';
 //rutas
 const authRoutes = require('./routes/authRoutes');
 app.use('/', authRoutes);
