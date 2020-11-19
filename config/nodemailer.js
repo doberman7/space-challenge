c81e14fe1f4b0a7ad19cb19d6d941aa30cba8294
Ivan Rubio <ivanrubioangeles@gmail.com>
2020-11-19 11:47:38 -0600

    Done

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -62,7 +62,7 @@ exports.emailSendChallenge = ( email, challenge) => {
       <p class="title">
         The challenge is beat the score of ${challenge.score} under ${challenge.time} seconds
       </p>
-      <p>Probe you can beat the challenge signIn on Space challenge at</p>
+      <p>Proof you can beat the challenge signin up on Space challenge at</p>
       <a href="https://space-demons3.herokuapp.com/">Space Challenge</a>
 
       </body>
